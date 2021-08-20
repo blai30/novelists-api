d66cb57e65cb52373fd0d4dd878785a77a9e9476
Brian Lai <brianlai30@outlook.com>
2021-08-20 00:14:10 -0700

    refactor: add Dapper to services and move DbContext configuration

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,17 +1,29 @@
-﻿using System.Reflection;
+﻿using System.Data;
+using System.Reflection;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using NovelistsApi.Infrastructure.Persistence;
+using Npgsql;
 
 namespace NovelistsApi.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
-            services.AddDbContextFactory<NovelistsDbContext>();
+
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+            services.AddTransient<IDbConnection>(_ => new NpgsqlConnection(connectionString));
+
+            services.AddDbContextFactory<NovelistsDbContext>(builder =>
+            {
+                builder.UseNpgsql(connectionString);
+                // Map PascalCase POCO properties to snake_case MySQL tables and columns.
+                builder.UseSnakeCaseNamingConvention();
+            });
 
             return services;
         }
